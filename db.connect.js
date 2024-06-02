685921b2a1cf2f87d71434b1c7f9c536a4e63a63
pokhrelyadav <pokhrelyadav412@gmail.com>
2024-06-02 11:30:04 +0545

    Added alternative .then.catch dbconnect method

--- db.connect.js
+++ db.connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://mongodbyadav123:mongodbyadav123@pokhrelcluster.sooa4rs.mongodb.net/udemy?retryWrites=true&w=majority&appName=pokhrelcluster"
+      "mongodb+srv://mongodbyadav123:mongodbyadav123@pokhrelcluster.sooa4rs.mongodb.net/amazon?retryWrites=true&w=majority&appName=pokhrelcluster"
     );
     console.log("DB connection successful");
   } catch (error) {
@@ -12,3 +12,20 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+// import mongoose from "mongoose";
+// const connectDB = async () => {
+//   await mongoose
+//     .connect(
+//       "mongodb+srv://mongodbyadav123:mongodbyadav123@pokhrelcluster.sooa4rs.mongodb.net/amazon?retryWrites=true&w=majority&appName=pokhrelcluster"
+//     )
+//     .then(() => {
+//       console.log("db connect successful");
+//     })
+//     .catch((error) => {
+//       console.log(unsuccessful)
+//       console.log(error.message);
+//     });
+// };
+
+// export default connectDB;
